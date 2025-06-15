d8179221377e2ad74554f5a343ea4f7089fea322
MihajloVidanovic <vidanovicmihajlo07@gmail.com>
2025-06-15 16:33:55 +0200

    resen 04_pravougaonik

--- Geometrija/04_pravougaonik.cpp
+++ Geometrija/04_pravougaonik.cpp
@@ -11,18 +11,21 @@ int main()
     for (int i = 0; i < 4; i++) {
         cin >> p[i].first >> p[i].second;
     }
-    set<pair,
-    for (int i = 0; i < 4; i++) {
-        pair<int, int> v1, v2;
-        v1.first = p[(i - 1 + 4) % 4].first - p[i].first;
-        v1.second = p[(i - 1 + 4) % 4].second - p[i].second;
-        v2.first = p[(i + 1 + 4) % 4].first - p[i].first;
-        v2.second = p[(i + 1 + 4) % 4].second - p[i].second;
-        if ((v1.first * v2.first) + (v1.second * v2.second) != 0) {
-            cout << "NE\n";
-            return 0;
+    while(next_permutation(p.begin(), p.end())) {
+        for (int i = 0; i < 4; i++) {
+            pair<int, int> v1, v2;
+            v1.first = p[(i - 1 + 4) % 4].first - p[i].first;
+            v1.second = p[(i - 1 + 4) % 4].second - p[i].second;
+            v2.first = p[(i + 1 + 4) % 4].first - p[i].first;
+            v2.second = p[(i + 1 + 4) % 4].second - p[i].second;
+            if ((v1.first * v2.first) + (v1.second * v2.second) != 0) {
+                goto nije_pravougaonik;
+            }
         }
+        cout << "DA\n";
+        return 0;
+        nije_pravougaonik: ;
     }
-    cout << "DA\n";
+    cout << "NE\n";
     return 0;
 }
